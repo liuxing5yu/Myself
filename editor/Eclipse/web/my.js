153cc5f0bff00aa2e43fdeafdae6040332939275
liuxing5yu <809534335@qq.com>
2017-11-29 20:06:07 +0800

    文件夹名字使用英文，调整了datatables的一些设置

--- editor/Eclipse/web/my.js
+++ editor/Eclipse/web/my.js
@@ -1,9 +1,41 @@
 $(function() {
-    $('#tb').DataTable();
+    $('#tb').DataTable({
+        language: {
+            "sProcessing": "处理中...",
+            "sLengthMenu": "显示 _MENU_ 项结果",
+            "sZeroRecords": "没有匹配结果",
+            "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+            "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
+            "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+            "sInfoPostFix": "",
+            "sSearch": "搜索:",
+            "sUrl": "",
+            "sEmptyTable": "表中数据为空",
+            "sLoadingRecords": "载入中...",
+            "sInfoThousands": ",",
+            "oPaginate": {
+                "sFirst": "首页",
+                "sPrevious": "上页",
+                "sNext": "下页",
+                "sLast": "末页"
+            },
+            "oAria": {
+                "sSortAscending": ": 以升序排列此列",
+                "sSortDescending": ": 以降序排列此列"
+            }
+        },
+        stripeClasses: ["odd", "even"], // 为奇偶行加上样式，兼容不支持CSS伪类的场合
+        dom: 'rt<"bottom"iflp<"clear">>',
+        stateSave: true,
+        searching : false, // 禁用原生搜索
+        ordering: false,
+        
+        "ajax": "data.json"
+    });
 
-    $('#saveRows').on('click',function(){
-        var trs=[];
-        $('#tb tbody tr').each(function(){
+    $('#saveRows').on('click', function() {
+        var trs = [];
+        $('#tb tbody tr').each(function() {
             trs.push(this.outerHTML);
         });
 
@@ -217,5 +249,3 @@ $(function() {
     }
 
 });
-
-
